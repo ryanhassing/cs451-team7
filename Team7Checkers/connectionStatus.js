e207c456dbad9369d31dd514c28d01b5bf81babc
Ryan Hassing <ryan.howardanton.hassing@gmail.com>
2018-03-10 17:56:42 -0500

    small changes

--- Team7Checkers/connectionStatus.js
+++ Team7Checkers/connectionStatus.js
@@ -1,28 +1,28 @@
 var WebSocket = require('ws');
+var BSON = require('bson');
+var bson = new BSON();
 
 // create socket
 const connection = new WebSocket('ws://10.250.9.50:4567/checkers');
 
-// connection opened
-connection.addEventListener('open', function(event) {
-  connection.send('Connected.');
-
-});
-
-connection.onopen = function() {
-  connection.send('Connected.');
-}
+// connection.onopen = function() {
+//   // connection.send('Connected.');
+// }
+// '{"msg": "connected"}';
 
 connection.onerror = function(error) {
   console.log(error);
 }
 
 connection.onmessage = function(event) {
-  console.log('Message: ', event.data);
+  console.log(event.data);
+
+  // console.log('Message: ', event.data.move[0]);
+  // if (event.data)
 }
 
 // once connection is opened, we can do stuff
-if (connection.readyState == 1) {
-  connection.send("hi");
-  connection.send("hi2");
-}
+// if (connection.readyState == 1) {
+//   connection.send("hi");
+//   connection.send("hi2");
+// }
