d1769f17f44bbc2f53fefa6ba3e3b3d14668edfb
ryanhassing <ryan.howardanton.hassing@gmail.com>
2018-03-15 21:58:11 -0400

    adding releases

---
+++ release/team-7-checkers-darwin-x64/team-7-checkers.app/Contents/Resources/app/connectionStatus.js
@@ -0,0 +1,28 @@
+var WebSocket = require('ws')
+var BSON = require('bson')
+var bson = new BSON()
+
+// create socket
+const connection = new WebSocket('ws://10.250.9.50:4567/checkers')
+
+// connection.onopen = function() {
+//   // connection.send('Connected.');
+// }
+// '{"msg": "connected"}';
+
+connection.onerror = function (error) {
+  console.log(error)
+}
+
+connection.onmessage = function (event) {
+  console.log(event.data)
+
+  // console.log('Message: ', event.data.move[0]);
+  // if (event.data)
+}
+
+// once connection is opened, we can do stuff
+// if (connection.readyState == 1) {
+//   connection.send("hi");
+//   connection.send("hi2");
+// }
